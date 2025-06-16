eca36218ae81da8111462596e59f2e6191a57eec
Rukayat08 <mustapharukayat08@gmail.com>
2025-06-16 20:56:56 +0100

    Create defenceanalysis.py

---
+++ defenceanalysis.py
@@ -0,0 +1,59 @@
+import pandas as pd 
+import streamlit as st 
+import plotly.express as px 
+import seaborn as sns
+import numpy as np 
+
+st.title ("DEFENCE ANALYSIS")
+#import csv file
+df = pd.read_csv("defending.csv")
+
+st.markdown("## Overview")
+st.markdown("### First Ten Observation")
+st.write(df.head(10))
+
+st.markdown("Last Ten Observation")
+st.write(df.tail(10))
+
+st.markdown("### Description of Numeric Data Types")
+st.write(df.describe())
+
+st.markdown("### Summary  of Table")
+st.write(df.shape)
+
+st.markdown("##Univariate Analysis")
+
+st.markdown("Clubs")
+st.write(df["club"].describe())
+
+st.markdown("Position")
+st.write(df["position"].describe())
+
+st.markdown("Players")
+st.write(df["player_name"].describe())
+
+st.markdown("Match Played")
+st.write(df["match_played"].describe())
+
+st.markdown("Total Won")
+st.write(df["t_won"].describe())
+
+st.markdown("Total Lost")
+st.write(df["t_lost"].describe())
+ 
+ st.markdown("Bar Chart Representation")
+ goals = px.bar(df["players"], y = "players", title = "Total goals lost")
+ st.plotly_chart(goals, use_container_width = True)
+
+ st.markdown("Bivariate Analysis")
+ 
+ st.markdown("### Club Vs Total Match Played")
+ df2 = px.histogram(df2, x = "club", y = "match_played", 
+ title = "Distribution of Club vs Match played")
+ st.plotly_chart(goals2, use_container_width = True )
+
+
+
+
+
+st.
